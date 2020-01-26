54eb2cce7aaccb7b50670d9717d5d549efb04ed9
Dhruvin Donda <dhruvindonda@gmail.com>
2020-01-26 09:47:21 +0530

    1

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:torch/torch.dart';
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'dart:async';
@@ -61,12 +63,25 @@ class DrawerOnly extends StatelessWidget{
                            }
                          ),
                          ListTile(
-                           title:Text('practical 3'),
+                           title:Text('practical 4'),
                            onTap: (){
                              Navigator.pop(context);
                              Navigator.push(context,new MaterialPageRoute(builder :(context)=>new Practical4()));
                            }
-                         )
+                         ),
+                          ListTile(
+                           title: Text('practical 5'),
+                         onTap: (){
+                            Navigator.pop(context);
+                            Navigator.push(context,new MaterialPageRoute(builder :(context)=>new Practical5())); 
+                         },),
+                         ListTile(
+                           title: Text('practical 6'),
+                         onTap: (){
+                            Navigator.pop(context);
+                            Navigator.push(context,new MaterialPageRoute(builder :(context)=>new Practical6())); 
+                         },)
+                          
                           ],
                         
                          ),
@@ -235,6 +250,124 @@ class _Practical4State extends State<Practical4>{
           ],
         ) ,
         )
+    );
+  }
+}
+
+String user = '0';
+String password = '0';
+bool _isButtonDisabeled = false;
+String loginmessage = '';
+int attempt = 3;
+Color bgcolor = Colors.white;
+
+class Practical5 extends StatefulWidget {
+  Practical5({Key key}) : super(key: key);
+
+  @override
+  _Practical5State createState() => _Practical5State();
+}
+
+class _Practical5State extends State<Practical5> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Practical 5"),
+      ),
+      body: ListView(
+        children: <Widget>[
+          TextField(
+            decoration: InputDecoration(
+              icon: Icon(Icons.email),
+              border: OutlineInputBorder(),
+              hintText: "Enter user name or email",
+            ),
+            onChanged: (val){
+              user = val;
+            },
+          ),
+          TextField(
+            decoration: InputDecoration(
+              icon: Icon(Icons.lock),
+              border: OutlineInputBorder(),
+              hintText: "Enter Password",
+              
+            ),
+            obscureText: true,
+            onChanged: (val){
+              password = val;
+            },
+          ),
+          RaisedButton(
+            child: Text("log in"),
+            onPressed: _isButtonDisabeled ? null : succes,
+          ),
+          Text(loginmessage,
+          style: TextStyle(
+            backgroundColor: bgcolor,
+          ),),
+          Text("Attempt remaining : "+attempt.toString()),
+          FloatingActionButton(
+            tooltip: 'close App',
+            child: Icon(Icons.close),
+            onPressed: (){
+              exit(0);
+            },
+          )
+        ],
+      ),
+    );
+  }
+  void succes(){
+    setState(() {
+      if(user=='17it123'){
+        if(password=='061199'){
+          bgcolor = Colors.green;
+          loginmessage = "login successful";
+          attempt=3;
+        }
+        else{
+          bgcolor = Colors.red;
+          loginmessage = "Login unsuccessful";
+          attempt--;
+        }
+        
+      }
+      else{
+        bgcolor = Colors.red;
+          loginmessage = "Login unsuccessful";
+          attempt--;
+      }
+      if(attempt==0){
+          _isButtonDisabeled = true;
+      }
+    });
+  }
+}
+class Practical6 extends StatelessWidget {
+  const Practical6({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("practical 6"),
+      ),
+      body: Center(
+        child: ListView(
+          children: <Widget>[
+            RaisedButton(
+              child: Text("ON"),
+              onPressed: Torch.turnOn,
+            ),
+            RaisedButton(
+              child: Text("OFF"),
+              onPressed: Torch.turnOff,
+            )
+          ],
+        ),
+      ),
     );
   }
 }
